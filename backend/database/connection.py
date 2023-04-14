59dc55752205f85f438df17d501ae6c3e12dcaad
DimitarP <dpolihronov1@gmail.com>
2023-04-14 14:23:52 +0300

    add connection.py to connect to database renamed data, skeleton folders updated overall structure

---
+++ backend/database/connection.py
@@ -0,0 +1,18 @@
+from mariadb import connect, Error
+from mariadb.connections import Connection
+from backend.config.config import settings
+
+
+def get_connection() -> Connection:
+    try:
+        connection = connect(
+            user=settings.database_username,
+            password=settings.database_password,
+            host=settings.database_hostname,
+            port=int(settings.database_port),
+            database=settings.database_name
+        )
+        return connection
+    except Error as e:
+        print(f"Error connecting to the database: {e}")
+        raise
--- backend/database/database.py
+++ backend/database/database.py
@@ -1,20 +1,8 @@
-from mariadb import connect
-from mariadb.connections import Connection
-from skeleton.config.config import settings
-
-
-def _get_connection() -> Connection:
-    return connect(
-        user=settings.database_username,
-        password=settings.database_password,
-        host=settings.database_hostname,
-        port=int(settings.database_port),
-        database=settings.database_name
-    )
+from backend.database.connection import get_connection
 
 
 def read_query(sql: str, sql_params=()):
-    with _get_connection() as conn:
+    with get_connection() as conn:
         cursor = conn.cursor()
         cursor.execute(sql, sql_params)
 
@@ -22,7 +10,7 @@ def read_query(sql: str, sql_params=()):
 
 
 def insert_query(sql: str, sql_params=()) -> int:
-    with _get_connection() as conn:
+    with get_connection() as conn:
         cursor = conn.cursor()
         cursor.execute(sql, sql_params)
         conn.commit()
@@ -31,10 +19,9 @@ def insert_query(sql: str, sql_params=()) -> int:
 
 
 def update_query(sql: str, sql_params=()) -> bool:
-    with _get_connection() as conn:
+    with get_connection() as conn:
         cursor = conn.cursor()
         cursor.execute(sql, sql_params)
         conn.commit()
 
         return cursor.rowcount
-
