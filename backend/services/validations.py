0e01a01840f377fae369d07f463bf5fc97496735
DimitarP <dpolihronov1@gmail.com>
2023-04-20 16:14:34 +0300

    added more statuses to UpdateStatus class

--- backend/services/validations.py
+++ backend/services/validations.py
@@ -2,6 +2,12 @@ from enum import Enum
 
 
 class UpdateStatus(Enum):
-    SUCCESS = "success"
-    NOT_FOUND = "not_found"
-    NO_CHANGE = "no_change"
+    DUPLICATE_ENTRY = "Already Exists"
+    SUCCESS = "Success"
+    NOT_FOUND = "Not found"
+    NO_CHANGE = "No change"
+    LOCKED = "Locked"
+    NO_WRITE_ACCESS = "No write access"
+    NO_READ_ACCESS = "No read access"
+    BAD_REQUEST = "Bad request"
+    ADMIN_REQUIRED = "Requires Admin Access!"
