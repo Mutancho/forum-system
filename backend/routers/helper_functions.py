5b2d283d46f40abc87689a3aed3082133351b165
DimitarP <dpolihronov1@gmail.com>
2023-04-20 16:15:58 +0300

    (added more http_validations func and added Constants class removed handle_updates) helper_functions.py

--- backend/routers/helper_functions.py
+++ backend/routers/helper_functions.py
@@ -1,17 +1,16 @@
+from enum import Enum
 from services.validations import UpdateStatus
 from fastapi import HTTPException, status
 
-CATEGORY = "Category"
-TOPIC = "Topic"
-REPLIES = "Reply"
 
-
-def handle_updates(update_status: UpdateStatus, info_type: str):
-    if update_status == UpdateStatus.NOT_FOUND:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{info_type} not found")
-    elif update_status == UpdateStatus.NO_CHANGE:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"{info_type} already in the desired state")
-    return update_status
+class Constants(Enum):
+    CATEGORY = "Category"
+    TOPIC = "Topic"
+    REPLY = "Reply"
+    READ = "read"
+    WRITE = "write"
+    GRANTED = 1
+    REVOKED = 0
 
 
 def query_filters(data, key: str, search: str | None = None, sort: str | None = None,
@@ -31,4 +30,26 @@ def query_filters(data, key: str, search: str | None = None, sort: str | None = 
 
     if limit:
         data = data[:limit]
+    return data
+
+
+def http_validations(data, info_type: Constants):
+    if data == UpdateStatus.SUCCESS:
+        return UpdateStatus.SUCCESS
+    if data == UpdateStatus.NOT_FOUND:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{info_type} not found")
+    if data == UpdateStatus.NO_CHANGE:
+        return UpdateStatus.NO_CHANGE
+    if data == UpdateStatus.LOCKED:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"{info_type} is locked")
+    if data == UpdateStatus.NO_WRITE_ACCESS:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"{info_type} write access denied")
+    if data == UpdateStatus.NO_READ_ACCESS:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"{info_type} read access denied")
+    if data == UpdateStatus.BAD_REQUEST:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Failed to create {info_type}")
+    if data == UpdateStatus.ADMIN_REQUIRED:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=UpdateStatus.ADMIN_REQUIRED)
+    if data == UpdateStatus.DUPLICATE_ENTRY:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Entry already exists")
     return data
