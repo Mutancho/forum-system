fca5b877c43298606bd4f5fc291651fc86735012
DimitarP <dpolihronov1@gmail.com>
2023-04-22 09:22:09 +0300

    add router_reply

--- backend/main.py
+++ backend/main.py
@@ -5,10 +5,12 @@ from routers.users import router_user
 from routers.categories import router_category
 from routers.messages import router_message
 from routers.topics import router_topic
+from routers.replies import router_reply
 
 app = FastAPI()
 app.include_router(router_user)
 app.include_router(router_category)
 app.include_router(router_message)
 app.include_router(router_topic)
+app.include_router(router_reply)
 app.include_router(router_reply)
