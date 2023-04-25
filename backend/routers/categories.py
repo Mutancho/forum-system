cf406a6ffa27f85bb9fcfe8e33af177b427b7491
DimitarP <dpolihronov1@gmail.com>
2023-04-25 19:15:29 +0300

    add search, sort, filter to get_categories()

--- backend/routers/categories.py
+++ backend/routers/categories.py
@@ -9,9 +9,11 @@ router_category = APIRouter(prefix="/categories", tags=["Categories"])
 
 
 @router_category.get("/")
-def get_categories():
+def get_categories(search: str | None = None, sort: str | None = None,
+                   skip: int | None = None, limit: int | None = None):
     data = get_all()
-    return data
+    filtered_data = query_filters(data, key="name", search=search, sort=sort, skip=skip, limit=limit)
+    return filtered_data
 
 
 @router_category.get("/{category_id}")
