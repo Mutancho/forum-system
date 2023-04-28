91d13023312b04e59bf78b63da54d3440dd42eb3
DimitarP <dpolihronov1@gmail.com>
2023-04-28 23:08:26 +0300

    add asynchronous functionality database pooling

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,22 @@ from routers.categories import router_category
 from routers.messages import router_message
 from routers.topics import router_topic, router_all_topics
 from routers.replies import router_reply
+from database.connection import init_db, get_connection
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    await init_db()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    pool = await get_connection()
+    await pool.close()
+
+
 app.include_router(router_user)
 app.include_router(router_category)
 app.include_router(router_message)
