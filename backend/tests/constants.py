61e9de038296558fe929159c3dbbe8a6dec636c2
DimitarP <dpolihronov1@gmail.com>
2023-04-28 14:41:50 +0300

    add more unit test constants to constants.py

--- backend/tests/constants.py
+++ backend/tests/constants.py
@@ -9,18 +9,27 @@ CATEGORY_ID_1 = 1
 CATEGORY_ID_2 = 2
 CATEGORY_1 = Category(id=1, name="Category 1")
 CATEGORY_2 = Category(id=2, name="Category 2")
+CATEGORY_NAME_1 = "TestCategory"
+CATEGORY_NAME_2 = "TestCategory2"
 ALL_CATEGORIES = [CATEGORY_1, CATEGORY_2]
 INVALID_CATEGORY_ID = 999
 PRIVATE_CATEGORY = ((1,),)
 NOT_PRIVATE_CATEGORY = ((0,),)
 READ_ACCESS_GRANTED = ((1,),)
 READ_ACCESS_DENIED = ((0,),)
-TOKEN = "dummy_token"
-TOPIC_NAME_1 = 'TestTopic1'
-TOPIC_NAME_2 = 'TestTopic2'
-CATEGORY_NAME_1 = "TestCategory"
-CATEGORY_NAME_2 = "TestCategory2"
-USER_ID_1 = 1
-USER_ID_2 = 1
 WRITE_ACCESS_GRANTED = ((1,),)
 WRITE_ACCESS_DENIED = ((0,),)
+
+TOKEN = "dummy_token"
+TOPIC_ID_1 = 1
+TOPIC_NAME_1 = 'TestTopic1'
+TOPIC_NAME_2 = 'TestTopic2'
+TOPIC_TITLE = 'Test topic'
+TOPIC_CONTENT = 'Test topic content'
+REPLY_ID_1 = 1
+REPLY_CONTENT = 'Test reply content'
+REPLY_ID_2 = 2
+BEST_REPLY_ID = 1
+
+USER_ID_1 = 1
+USER_ID_2 = 1
