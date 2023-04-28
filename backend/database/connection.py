e2c031d1220c0852899b36671ded1fd59844c202
DimitarP <dpolihronov1@gmail.com>
2023-04-28 22:35:13 +0300

    add error checking for get_connection function

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,18 +1,22 @@
 from mariadb import connect, Error
 from mariadb.connections import Connection
 from config.config import settings
+import time
 
 
-def get_connection() -> Connection:
-    try:
-        connection = connect(
-            user=settings.database_username,
-            password=settings.database_password,
-            host=settings.database_hostname,
-            port=settings.database_port,
-            database=settings.database_name
-        )
-        return connection
-    except Error as e:
-        print(f"Error connecting to the database: {e}")
-        raise
+def get_connection(max_retries=5, delay=5) -> Connection:
+    for _ in range(max_retries):
+        try:
+            connection = connect(
+                user=settings.database_username,
+                password=settings.database_password,
+                host=settings.database_hostname,
+                port=settings.database_port,
+                database=settings.database_name
+            )
+            return connection
+        except Error as e:
+            print(f"Error connecting to the database: {e}")
+            time.sleep(delay)
+    raise Exception("Could not connect to the database after max retries")
+
