0e93e2c4f65fb6b129a58f8ea07aefd0e06009aa
AntonNikiforov <nikifor.anto@gmail.com>
2023-04-28 14:53:09 +0300

    adding requirements

--- backend/tests/services/message_service_test.py
+++ backend/tests/services/message_service_test.py
@@ -23,8 +23,10 @@ class UserService_Should(unittest.TestCase):
     @patch('services.message_service.read_query', autospec=True)
     def test_getAllMyConversations_returnsListOfDisplayUser(self, mock_read_query):
         message_service.oauth2.get_current_user = lambda token: 1
-        mock_read_query.side_effect = [[(1,),(1,)],[(2,),(2,)],[(1, 'username1', 'email@us1.com','password1', 'user', datetime.datetime.strptime('04/14/2023, 13:58:43',"%m/%d/%Y, %H:%M:%S")),
-                                                                (2, 'username2', 'email@us2.com','password2','user', datetime.datetime.strptime('04/14/2023, 14:30:43',"%m/%d/%Y, %H:%M:%S"))]]
+        mock_read_query.side_effect = \
+            [[(1,),(1,)],[(2,),(2,)],
+            [(1, 'username1', 'email@us1.com','password1', 'user', datetime.datetime.strptime('04/14/2023, 13:58:43',"%m/%d/%Y, %H:%M:%S")),
+            (2, 'username2', 'email@us2.com','password2','user', datetime.datetime.strptime('04/14/2023, 14:30:43',"%m/%d/%Y, %H:%M:%S"))]]
 
         result = message_service.get_all_my_conversations('token')
         expected = [DisplayUser(id=1, username='username1', email='email@us1.com'),
@@ -34,9 +36,10 @@ class UserService_Should(unittest.TestCase):
     @patch('services.message_service.read_query', autospec=True)
     def test_getAllMyConversationsWith_returnsDictOfMessages(self, mock_read_query):
         message_service.oauth2.get_current_user = lambda token: 1
-        mock_read_query.return_value = [(1,'my message',datetime.datetime.strptime('04/14/2023, 13:58:43',"%m/%d/%Y, %H:%M:%S"),2),
-                                        (2,'his message',datetime.datetime.strptime('04/14/2023, 13:59:43',"%m/%d/%Y, %H:%M:%S"),1),
-                                        (1,'my message',datetime.datetime.strptime('04/14/2023, 14:58:43',"%m/%d/%Y, %H:%M:%S"),2)]
+        mock_read_query.return_value = \
+            [(1,'my message',datetime.datetime.strptime('04/14/2023, 13:58:43',"%m/%d/%Y, %H:%M:%S"),2),
+             (2,'his message',datetime.datetime.strptime('04/14/2023, 13:59:43',"%m/%d/%Y, %H:%M:%S"),1),
+             (1,'my message',datetime.datetime.strptime('04/14/2023, 14:58:43',"%m/%d/%Y, %H:%M:%S"),2)]
 
 
         result = message_service.get_all_my_conversations_with('token',2)
