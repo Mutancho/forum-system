732d3a7d8a794b6614e7a6df631df1787b2b918e
DimitarP <dpolihronov1@gmail.com>
2023-04-29 19:30:28 +0300

    add asynchronous testing

--- backend/tests/services/topics_test.py
+++ backend/tests/services/topics_test.py
@@ -10,7 +10,7 @@ from schemas.topic import TopicsTimeStamps, TopicWithContent, TopicBestReply
 class TestGetAll_Should(TestCase):
 
     @patch('services.topic_service.read_query')
-    def test_get_all(self, mock_read_query):
+    async def test_get_all(self, mock_read_query):
         # Arrange
         mock_read_query.return_value = [
             (1, 'topic1', date(2023, 1, 1), date(2023, 1, 2)),
@@ -18,7 +18,7 @@ class TestGetAll_Should(TestCase):
         ]
 
         # Act
-        result = get_all()
+        result = await get_all()
 
         # Assert
         self.assertEqual(len(result), 2)
@@ -41,29 +41,29 @@ class TestDelete_Should(TestCase):
     @patch('services.topic_service.topic_exists', return_value=True)
     @patch('services.topic_service.get_current_user', return_value=C.USER_ID_1)
     @patch('services.topic_service.update_query')
-    def test_delete_success(self, mock_update_query, mock_get_current_user, mock_topic_exists):
-        result = delete(C.TOKEN, C.TOPIC_ID_1)
+    async def test_delete_success(self, mock_update_query, mock_get_current_user, mock_topic_exists):
+        result = await delete(C.TOKEN, C.TOPIC_ID_1)
         self.assertEqual(result, UpdateStatus.SUCCESS)
         mock_update_query.assert_called_once_with("DELETE FROM topics WHERE id = ?", (C.TOPIC_ID_1,))
 
     @patch('services.topic_service.topic_exists', return_value=False)
-    def test_delete_not_found(self, mock_topic_exists):
-        result = delete(C.TOKEN, C.TOPIC_ID_1)
+    async def test_delete_not_found(self, mock_topic_exists):
+        result = await delete(C.TOKEN, C.TOPIC_ID_1)
         self.assertEqual(result, UpdateStatus.NOT_FOUND)
 
     @patch('services.topic_service.topic_exists', return_value=True)
     @patch('services.topic_service.get_current_user', return_value=None)
     @patch('services.topic_service.is_admin', return_value=False)
-    def test_delete_bad_request(self, mock_is_admin, mock_get_current_user, mock_topic_exists):
-        result = delete(C.TOKEN, C.TOPIC_ID_1)
+    async def test_delete_bad_request(self, mock_is_admin, mock_get_current_user, mock_topic_exists):
+        result = await delete(C.TOKEN, C.TOPIC_ID_1)
         self.assertEqual(result, UpdateStatus.BAD_REQUEST)
 
     @patch('services.topic_service.topic_exists', return_value=True)
     @patch('services.topic_service.get_current_user', return_value=None)
     @patch('services.topic_service.is_admin', return_value=True)
     @patch('services.topic_service.update_query')
-    def test_delete_success_admin(self, mock_update_query, mock_is_admin, mock_get_current_user, mock_topic_exists):
-        result = delete(C.TOKEN, C.TOPIC_ID_1)
+    async def test_delete_success_admin(self, mock_update_query, mock_is_admin, mock_get_current_user, mock_topic_exists):
+        result = await delete(C.TOKEN, C.TOPIC_ID_1)
         self.assertEqual(result, UpdateStatus.SUCCESS)
         mock_update_query.assert_called_once_with("DELETE FROM topics WHERE id = ?", (C.TOPIC_ID_1,))
 
@@ -71,9 +71,9 @@ class TestDelete_Should(TestCase):
 class TestUpdate_Should(TestCase):
     @patch('services.topic_service.get_current_user', return_value=C.USER_ID_1)
     @patch('services.topic_service.update_query', return_value=1)
-    def test_update_success(self, mock_update_query, mock_get_current_user):
+    async def test_update_success(self, mock_update_query, mock_get_current_user):
         topic = TopicWithContent(id=None, title=C.TOPIC_TITLE, content=C.TOPIC_CONTENT)
-        result = update(C.TOKEN, C.TOPIC_ID_1, topic)
+        result = await update(C.TOKEN, C.TOPIC_ID_1, topic)
         self.assertEqual(result, UpdateStatus.SUCCESS)
         mock_update_query.assert_called_once_with(
             "UPDATE topics SET title = ?, content = ? WHERE id = ? AND user_id = ?",
@@ -81,71 +81,71 @@ class TestUpdate_Should(TestCase):
 
     @patch('services.topic_service.get_current_user', return_value=C.USER_ID_1)
     @patch('services.topic_service.update_query', return_value=0)
-    def test_update_not_found(self, mock_update_query, mock_get_current_user):
+    async def test_update_not_found(self, mock_update_query, mock_get_current_user):
         topic = TopicWithContent(id=None, title=C.TOPIC_TITLE, content=C.TOPIC_CONTENT)
-        result = update(C.TOKEN, C.TOPIC_ID_1, topic)
+        result = await update(C.TOKEN, C.TOPIC_ID_1, topic)
         self.assertEqual(result, UpdateStatus.NOT_FOUND)
 
 
 class TestLock_Should(TestCase):
     @patch('services.topic_service.is_admin', return_value=True)
     @patch('services.topic_service.update_query', return_value=1)
-    def test_lock_success(self, mock_update_query, mock_is_admin):
-        result = lock(C.TOKEN, C.TOPIC_ID_1)
+    async def test_lock_success(self, mock_update_query, mock_is_admin):
+        result = await lock(C.TOKEN, C.TOPIC_ID_1)
         self.assertEqual(result, UpdateStatus.SUCCESS)
         mock_update_query.assert_called_once_with("UPDATE topics SET locked = 1 WHERE id = ?", (C.TOPIC_ID_1,))
 
     @patch('services.topic_service.is_admin', return_value=True)
     @patch('services.topic_service.update_query', return_value=0)
-    def test_lock_not_found(self, mock_update_query, mock_is_admin):
-        result = lock(C.TOKEN, C.TOPIC_ID_1)
+    async def test_lock_not_found(self, mock_update_query, mock_is_admin):
+        result = await lock(C.TOKEN, C.TOPIC_ID_1)
         self.assertEqual(result, UpdateStatus.NOT_FOUND)
 
     @patch('services.topic_service.is_admin', return_value=False)
-    def test_lock_not_admin(self, mock_is_admin):
-        result = lock(C.TOKEN, C.TOPIC_ID_1)
+    async def test_lock_not_admin(self, mock_is_admin):
+        result = await lock(C.TOKEN, C.TOPIC_ID_1)
         self.assertIsNone(result)
 
     @patch('services.topic_service.is_admin', return_value=True)
     @patch('services.topic_service.update_query', return_value=1)
-    def test_unlock_success(self, mock_update_query, mock_is_admin):
-        result = unlock(C.TOKEN, C.TOPIC_ID_1)
+    async def test_unlock_success(self, mock_update_query, mock_is_admin):
+        result = await unlock(C.TOKEN, C.TOPIC_ID_1)
         self.assertEqual(result, UpdateStatus.SUCCESS)
         mock_update_query.assert_called_once_with("UPDATE topics SET locked = 0 WHERE id = ?", (C.TOPIC_ID_1,))
 
     @patch('services.topic_service.is_admin', return_value=True)
     @patch('services.topic_service.update_query', return_value=0)
-    def test_unlock_not_found(self, mock_update_query, mock_is_admin):
-        result = unlock(C.TOKEN, C.TOPIC_ID_1)
+    async def test_unlock_not_found(self, mock_update_query, mock_is_admin):
+        result = await unlock(C.TOKEN, C.TOPIC_ID_1)
         self.assertEqual(result, UpdateStatus.NOT_FOUND)
 
     @patch('services.topic_service.is_admin', return_value=False)
-    def test_unlock_not_admin(self, mock_is_admin):
-        result = unlock(C.TOKEN, C.TOPIC_ID_1)
+    async def test_unlock_not_admin(self, mock_is_admin):
+        result = await unlock(C.TOKEN, C.TOPIC_ID_1)
         self.assertIsNone(result)
 
 
 class TestBestReply_Should(TestCase):
-    def setUp(self):
+    async def setUp(self):
         self.topic_best_reply = TopicBestReply(best_reply_id=C.BEST_REPLY_ID)
 
     @patch('services.topic_service.get_current_user', return_value=C.USER_ID_1)
     @patch('services.topic_service.update_query', return_value=0)
     @patch('services.topic_service.exists_by_id', return_value=False)
-    def test_set_best_reply_not_found(self, mock_exists_by_id, mock_update_query, mock_get_current_user):
-        result = set_best_reply(C.TOKEN, C.TOPIC_ID_1, self.topic_best_reply)
+    async def test_set_best_reply_not_found(self, mock_exists_by_id, mock_update_query, mock_get_current_user):
+        result = await set_best_reply(C.TOKEN, C.TOPIC_ID_1, self.topic_best_reply)
         self.assertEqual(result, UpdateStatus.NOT_FOUND)
 
     @patch('services.topic_service.get_current_user', return_value=C.USER_ID_1)
     @patch('services.topic_service.update_query', return_value=0)
     @patch('services.topic_service.exists_by_id', return_value=True)
-    def test_set_best_reply_no_permission(self, mock_exists_by_id, mock_update_query, mock_get_current_user):
-        result = set_best_reply(C.TOKEN, C.TOPIC_ID_1, self.topic_best_reply)
+    async def test_set_best_reply_no_permission(self, mock_exists_by_id, mock_update_query, mock_get_current_user):
+        result = await set_best_reply(C.TOKEN, C.TOPIC_ID_1, self.topic_best_reply)
         self.assertEqual(result, UpdateStatus.NOT_FOUND)
 
     @patch('services.topic_service.get_current_user', return_value=C.USER_ID_1)
     @patch('services.topic_service.update_query', return_value=1)
     @patch('services.topic_service.exists_by_id', return_value=True)
-    def test_set_best_reply_success(self, mock_exists_by_id, mock_update_query, mock_get_current_user):
-        result = set_best_reply(C.TOKEN, C.TOPIC_ID_1, self.topic_best_reply)
+    async def test_set_best_reply_success(self, mock_exists_by_id, mock_update_query, mock_get_current_user):
+        result = await set_best_reply(C.TOKEN, C.TOPIC_ID_1, self.topic_best_reply)
         self.assertEqual(result, UpdateStatus.SUCCESS)
