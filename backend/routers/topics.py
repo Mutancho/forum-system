c007e07373f94aa9e3025033eef5bef8f90362aa
DimitarP <dpolihronov1@gmail.com>
2023-04-29 19:22:52 +0300

    add asynchronous functionality

--- backend/routers/topics.py
+++ backend/routers/topics.py
@@ -10,59 +10,59 @@ router_all_topics = APIRouter(tags=["Topics"])
 
 
 @router_all_topics.get("/topics")
-def get_all_topics(search: str | None = None, sort: str | None = None,
-                   skip: int | None = None, limit: int | None = None):
-    data = get_all()
+async def get_all_topics(search: str | None = None, sort: str | None = None,
+                         skip: int | None = None, limit: int | None = None):
+    data = await get_all()
     filtered_data = query_filters(data, key="title", search=search, sort=sort, skip=skip, limit=limit)
     return filtered_data
 
 
 @router_topic.get("/{topic_id}")
-def get_by_id_with_replies(category_id: int, topic_id: int, auth: str = Header(alias="Authorization")):
+async def get_by_id_with_replies(category_id: int, topic_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    topics_with_replies = get_topic_by_id_with_replies(token, category_id, topic_id)
+    topics_with_replies = await get_topic_by_id_with_replies(token, category_id, topic_id)
     return http_validations(topics_with_replies, C.TOPIC)
 
 
 @router_topic.post("/")
-def create_topic(category_id: int, topic: TopicWithUserAndCategoryWithContent,
+async def create_topic(category_id: int, topic: TopicWithUserAndCategoryWithContent,
                  auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    created_topic = create(token, category_id, topic)
+    created_topic = await create(token, category_id, topic)
     topic.user_id = get_current_user(token)
     return http_validations(created_topic, C.CATEGORY)
 
 
 @router_topic.delete("/{topic_id}")
-def delete_topic(topic_id: int, auth: str = Header(alias="Authorization")):
+async def delete_topic(topic_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    removed_topic = delete(token, topic_id)
+    removed_topic = await delete(token, topic_id)
     return http_validations(removed_topic, C.TOPIC)
 
 
 @router_topic.put("/{topic_id}")
-def topic_update(topic_id: int, topic: TopicWithContent, auth: str = Header(alias="Authorization")):
+async def topic_update(topic_id: int, topic: TopicWithContent, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    updated_topic = update(token, topic_id, topic)
+    updated_topic = await update(token, topic_id, topic)
     return http_validations(updated_topic, C.TOPIC)
 
 
 @router_topic.put("/best_reply/{topic_id}")
-def best_reply(topic_id: int, topic: TopicBestReply, auth: str = Header(alias="Authorization")):
+async def best_reply(topic_id: int, topic: TopicBestReply, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    updated_topic = set_best_reply(token, topic_id, topic)
+    updated_topic = await set_best_reply(token, topic_id, topic)
     return http_validations(updated_topic, C.TOPIC)
 
 
 @router_topic.put("/lock/{topic_id}")
-def lock_topic(topic_id: int, auth: str = Header(alias="Authorization")):
+async def lock_topic(topic_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    update_status = lock(token, topic_id)
+    update_status = await lock(token, topic_id)
     return http_validations(update_status, C.TOPIC)
 
 
 @router_topic.put("/unlock/{topic_id}")
-def unlock_topic(topic_id: int, auth: str = Header(alias="Authorization")):
+async def unlock_topic(topic_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    update_status = unlock(token, topic_id)
+    update_status = await unlock(token, topic_id)
     return http_validations(update_status, C.TOPIC)
--- backend/services/topic_service.py
+++ backend/services/topic_service.py
@@ -1,6 +1,6 @@
 from database.database_queries import read_query, insert_query, update_query
 from schemas.topic import TopicWithUserAndCategoryWithContent, TopicWithContent, TopicWithReplies, TopicBestReply, \
-    TopicsTimeStamps
+    TopicsTimeStamps, TopicWithBestReply
 from schemas.reply import BaseReply
 from services.user_service import is_admin
 from services.validations import UpdateStatus
@@ -10,98 +10,126 @@ from services.category_service import is_category_private, category_read_restric
     category_write_restriction_applies, is_category_locked
 
 
-def get_all():
-    query = read_query("SELECT id, title, created_at, updated_at from topics")
+async def get_all():
+    query = await read_query("SELECT id, title, created_at, updated_at from topics")
     return [TopicsTimeStamps(id=topic_id, title=title, created_at=created_at, updated_at=updated_at) for
             topic_id, title, created_at, updated_at in query]
 
 
-def get_topic_by_id_with_replies(token: str, category_id: int, topic_id: int) -> TopicWithReplies | UpdateStatus:
+async def get_topic_by_id_with_replies(token: str, category_id: int, topic_id: int) -> TopicWithReplies | UpdateStatus:
     user_id = get_current_user(token)
-    topic_data = topic_exists(topic_id)
+    topic_data = await topic_exists(topic_id)
     if not topic_data:
         return UpdateStatus.NOT_FOUND
-
-    if not is_admin(token):
-        if not category_exists(category_id):
+    if_exists = await category_exists(category_id)
+    if not await is_admin(token):
+        if not if_exists:
             return UpdateStatus.NOT_FOUND
-        if is_category_private(category_id)[0][0] and not category_read_restriction_applies(user_id)[0][0]:
+        if_private = await is_category_private(category_id)
+        if_read_restrictions = await category_read_restriction_applies(user_id)
+        if if_private[0][0] and not if_read_restrictions[0][0]:
             return UpdateStatus.NO_READ_ACCESS
 
-    replies_data = read_query("SELECT id, content FROM replies WHERE topic_id = ?", (topic_id,))
-    replies = [BaseReply(id=id, content=content) for id, content in replies_data]
+    replies_data = await read_query("SELECT id, content FROM replies WHERE topic_id = %s", (topic_id,))
+    replies = [BaseReply(id=reply_id, content=content) for reply_id, content in replies_data]
     return TopicWithReplies(topic=topic_data, replies=replies)
 
 
-def create(token: str, category_id: int, topic: TopicWithUserAndCategoryWithContent):
+async def create(token: str, category_id: int, topic: TopicWithUserAndCategoryWithContent):
     user_id = get_current_user(token)
-    if not is_admin(token):
-        if is_category_private(category_id)[0][0] and not category_write_restriction_applies(user_id)[0][0]:
+    if not await is_admin(token):
+        if_private = await is_category_private(category_id)
+        write_restrictions = await category_write_restriction_applies(user_id)
+        if_locked = await is_category_locked(category_id)
+        if if_private[0][0] and not await write_restrictions[0][0]:
             return UpdateStatus.NO_WRITE_ACCESS
-        if is_category_locked(category_id)[0][0]:
+        if if_locked[0][0]:
             return UpdateStatus.LOCKED
 
-    is_locked = read_query("SELECT locked FROM categories WHERE id = ?", (category_id,))[0][0]
-    category = read_query("SELECT id FROM categories where id = ?", (category_id,))
-    if is_locked:
+    is_locked = await read_query("SELECT locked FROM categories WHERE id = %s", (category_id,))
+    category = await read_query("SELECT id FROM categories where id = %s", (category_id,))
+    if is_locked[0][0]:
         return UpdateStatus.LOCKED
     if category_id != category[0][0]:
         return UpdateStatus.BAD_REQUEST
-    generated_id = insert_query("INSERT INTO topics(title, content, user_id, category_id) VALUES(?,?,?, ?)",
-                                (topic.title, topic.content, user_id, category_id))
+    generated_id = await insert_query("INSERT INTO topics(title, content, user_id, category_id) VALUES(%s,%s,%s,%s)",
+                                      (topic.title, topic.content, user_id, category_id))
     topic.id = generated_id
     topic.category_id = category_id
     topic.user_id = user_id
     return topic
 
 
-def delete(token: str, topic_id: int) -> UpdateStatus:
-    if not topic_exists(topic_id):
+async def delete(token: str, topic_id: int) -> UpdateStatus:
+    if not await topic_exists(topic_id):
         return UpdateStatus.NOT_FOUND
-    if get_current_user(token) or is_admin(token):
-        update_query("DELETE FROM topics WHERE id = ?", (topic_id,))
+    if get_current_user(token) or await is_admin(token):
+        await update_query("DELETE FROM topics WHERE id = %s", (topic_id,))
         return UpdateStatus.SUCCESS
     return UpdateStatus.BAD_REQUEST
 
 
-def update(token: str, topic_id: int, topic: TopicWithContent) -> UpdateStatus:
+async def update(token: str, topic_id: int, topic: TopicWithContent) -> UpdateStatus:
     user_id = get_current_user(token)
-    updated_rows = update_query("UPDATE topics SET title = ?, content = ? WHERE id = ? AND user_id = ?",
-                                (topic.title, topic.content, topic_id, user_id))
-
+    if_admin = await is_admin(token)
+    if if_admin:
+        updated_rows = await update_query(
+            "UPDATE topics SET title = %s, content = %s WHERE id = %s",
+            (topic.title, topic.content, topic_id)
+        )
+    else:
+        updated_rows = await update_query(
+            "UPDATE topics SET title = %s, content = %s WHERE id = %s AND user_id = %s",
+            (topic.title, topic.content, topic_id, user_id)
+        )
     if updated_rows == 0:
-        return UpdateStatus.NOT_FOUND
+        return UpdateStatus.NO_CHANGE
+
     return UpdateStatus.SUCCESS
 
 
-def set_best_reply(token: str, topic_id: int, topic: TopicBestReply):
-    if not exists_by_id(topic.best_reply_id):
+async def set_best_reply(token: str, topic_id: int, topic: TopicBestReply) -> UpdateStatus:
+    exists = await exists_by_id(topic.best_reply_id)
+    if not exists:
         return UpdateStatus.NOT_FOUND
     user_id = get_current_user(token)
-    updated_rows = update_query("UPDATE topics SET best_reply_id = ? WHERE id = ? AND user_id = ?",
-                                (topic.best_reply_id, topic_id, user_id))
+    if_admin = await is_admin(token)
+
+    if if_admin:
+        updated_rows = await update_query(
+            "UPDATE topics SET best_reply_id = %s WHERE id = %s",
+            (topic.best_reply_id, topic_id)
+        )
+    else:
+        updated_rows = await update_query(
+            "UPDATE topics SET best_reply_id = %s WHERE id = %s AND user_id = %s",
+            (topic.best_reply_id, topic_id, user_id)
+        )
+
     if updated_rows == 0:
-        return UpdateStatus.NOT_FOUND
+        return UpdateStatus.NO_CHANGE
     return UpdateStatus.SUCCESS
 
 
-def lock(token: str, topic_id: int) -> UpdateStatus | None:
-    if is_admin(token):
-        updated_rows = update_query("UPDATE topics SET locked = 1 WHERE id = ?", (topic_id,))
+async def lock(token: str, topic_id: int) -> UpdateStatus | None:
+    if await is_admin(token):
+        updated_rows = await update_query("UPDATE topics SET locked = 1 WHERE id = %s", (topic_id,))
         return _update_helper(updated_rows, topic_id)
 
 
-def unlock(token: str, topic_id: int) -> UpdateStatus | None:
-    if is_admin(token):
-        updated_rows = update_query("UPDATE topics SET locked = 0 WHERE id = ?", (topic_id,))
+async def unlock(token: str, topic_id: int) -> UpdateStatus | None:
+    if await is_admin(token):
+        updated_rows = await update_query("UPDATE topics SET locked = 0 WHERE id = %s", (topic_id,))
         return _update_helper(updated_rows, topic_id)
 
 
-def topic_exists(topic_id: int) -> TopicWithContent | None:
-    data = read_query("SELECT id, title, content FROM topics where id = ?", (topic_id,))
+async def topic_exists(topic_id: int) -> TopicWithBestReply | None:
+    data = await read_query("SELECT id, title, content, best_reply_id, locked FROM topics where id = %s", (topic_id,))
     if not data:
         return None
-    return TopicWithContent(id=data[0][0], title=data[0][1], content=data[0][2])
+    print(data)
+    return TopicWithBestReply(id=data[0][0], title=data[0][1], content=data[0][2], best_reply_id=data[0][3],
+                              locked=data[0][4])
 
 
 def _update_helper(updated_rows: int, topic_id: int) -> UpdateStatus:
@@ -114,19 +142,19 @@ def _update_helper(updated_rows: int, topic_id: int) -> UpdateStatus:
     return UpdateStatus.SUCCESS
 
 
-def get_topic_by_id(topic_id: int) -> TopicWithContent | None:
-    data = read_query("SELECT id, title, content FROM topics WHERE id = ?", (topic_id,))
+async def get_topic_by_id(topic_id: int) -> TopicWithContent | None:
+    data = await read_query("SELECT id, title, content FROM topics WHERE id = %s", (topic_id,))
     if not data:
         return None
     return TopicWithContent(id=data[0][0], title=data[0][1], content=data[0][2])
 
 
-def is_topic_owner(topic_id, token):
+async def is_topic_owner(topic_id, token):
     auth_user_id = get_current_user(token)
-    data = read_query('''SELECT user_id FROM topics WHERE id = ? ''', (topic_id,))
+    data = await read_query('''SELECT user_id FROM topics WHERE id = %s ''', (topic_id,))
     user_id = data[0][0]
     return user_id == auth_user_id
 
 
-def is_topic_locked(topic_id: int):
-    return read_query("SELECT locked FROM topics WHERE id = ?", (topic_id,))
+async def is_topic_locked(topic_id: int):
+    return await read_query("SELECT locked FROM topics WHERE id = %s", (topic_id,))
