2383e75d59b3fe5282c02b4e33e764eeaa356d43
DimitarP <dpolihronov1@gmail.com>
2023-04-29 11:07:24 +0300

    add asynchronous functionality for in service and router

--- backend/routers/categories.py
+++ backend/routers/categories.py
@@ -17,10 +17,11 @@ async def get_categories(search: str | None = None, sort: str | None = None,
 
 
 @router_category.get("/{category_id}")
-def get_category_by_id(category_id: int, search: str | None = None, sort: str | None = None,
-                       skip: int | None = None, limit: int | None = None, auth: str = Header(alias="Authorization")):
+async def get_category_by_id(category_id: int, search: str | None = None, sort: str | None = None,
+                             skip: int | None = None, limit: int | None = None,
+                             auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    category_with_topics = get_category_by_id_with_topics(token, category_id)
+    category_with_topics = await get_category_by_id_with_topics(token, category_id)
 
     category_with_topics.topics = query_filters(category_with_topics.topics, key="title", search=search, sort=sort,
                                                 skip=skip,
@@ -29,75 +30,75 @@ def get_category_by_id(category_id: int, search: str | None = None, sort: str | 
 
 
 @router_category.get("/{category_id}/privileged_users")
-def get_privileged_users(category_id: int, auth: str = Header(alias="Authorization")):
+async def get_privileged_users(category_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    privileged_users = view_privileged_users(token, category_id)
+    privileged_users = await view_privileged_users(token, category_id)
     if not privileged_users:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
     return privileged_users
 
 
 @router_category.post("/", status_code=status.HTTP_201_CREATED)
-def create_category(category: Category, auth: str = Header(alias="Authorization")):
+async def create_category(category: Category, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    created_category = create(token, category)
+    created_category = await create(token, category)
     if created_category is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to create category")
     return category
 
 
 @router_category.post("/{category_id}/private/add/{user_id}")
-def add_user_to_private_category(category_id: int, user_id: int, auth: str = Header(alias="Authorization")):
+async def add_user_to_private_category(category_id: int, user_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    added_user = add_user_as_private_member(token, category_id, user_id)
+    added_user = await add_user_as_private_member(token, category_id, user_id)
     return http_validations(added_user, Constants.CATEGORY)
 
 
 @router_category.delete("/{category_id}/private/remove/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user_from_private_category(category_id: int, user_id: int, auth: str = Header(alias="Authorization")):
+async def delete_user_from_private_category(category_id: int, user_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    removed_user = remove_user_as_private_member(token, category_id, user_id)
+    removed_user = await remove_user_as_private_member(token, category_id, user_id)
     return http_validations(removed_user, Constants.CATEGORY)
 
 
 @router_category.delete("/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_category(category_id: int, auth: str = Header(alias="Authorization")):
+async def delete_category(category_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    deleted = delete(token, category_id)
+    deleted = await delete(token, category_id)
     if not deleted:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
 
 
 @router_category.put("/{category_id}")
-def update_category(category_id: int, category: Category, auth: str = Header(alias="Authorization")):
+async def update_category(category_id: int, category: Category, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    update_status = update(token, category_id, category)
+    update_status = await update(token, category_id, category)
     return http_validations(update_status, Constants.CATEGORY)
 
 
 @router_category.put("/lock/{category_id}")
-def lock_category(category_id: int, auth: str = Header(alias="Authorization")):
+async def lock_category(category_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    update_status = lock(token, category_id)
+    update_status = await lock(token, category_id)
     return http_validations(update_status, Constants.CATEGORY)
 
 
 @router_category.put("/unlock/{category_id}")
-def unlock_category(category_id: int, auth: str = Header(alias="Authorization")):
+async def unlock_category(category_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    update_status = unlock(token, category_id)
+    update_status = await unlock(token, category_id)
     return http_validations(update_status, Constants.CATEGORY)
 
 
 @router_category.put("/private/{category_id}")
-def make_category_private(category_id: int, auth: str = Header(alias="Authorization")):
+async def make_category_private(category_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    update_status = make_private(token, category_id)
+    update_status = await make_private(token, category_id)
     return http_validations(update_status, Constants.CATEGORY)
 
 
 @router_category.put("/non-private/{category_id}")
-def make_category_non_private(category_id: int, auth: str = Header(alias="Authorization")):
+async def make_category_non_private(category_id: int, auth: str = Header(alias="Authorization")):
     token = auth[8:-1]
-    update_status = make_non_private(token, category_id)
+    update_status = await make_non_private(token, category_id)
     return http_validations(update_status, Constants.CATEGORY)
--- backend/services/category_service.py
+++ backend/services/category_service.py
@@ -1,6 +1,6 @@
 from database.database_queries import read_query, insert_query, update_query
 from schemas.category import Category, CategoryWithTopics, PrivilegedCategoryUsers, CategoryOut
-from schemas.topic import TopicWithContent, BaseTopic
+from schemas.topic import BaseTopic
 from services.validations import UpdateStatus
 from utils.oauth2 import get_current_user
 from services.user_service import is_admin, exists_by_id
@@ -13,98 +13,102 @@ async def get_all():
             query]
 
 
-def get_category_by_id_with_topics(token: str, category_id: int):
+async def get_category_by_id_with_topics(token: str, category_id: int):
     user_id = get_current_user(token)
-    category_data = category_exists(category_id)
+    category_data = await category_exists(category_id)
 
     if not category_data:
         return UpdateStatus.NOT_FOUND
 
-    if not is_admin(token):
-        if is_category_private(category_id)[0][0] and not category_read_restriction_applies(user_id)[0][0]:
+    if not await is_admin(token):
+        is_private = await is_category_private(category_id)
+        read_restriction = await category_read_restriction_applies(user_id)
+        if is_private[0][0] and not read_restriction[0][0]:
             return UpdateStatus.NO_READ_ACCESS
 
-    topic_data = read_query("SELECT id, title FROM topics WHERE category_id = ?", (category_id,))
+    topic_data = await read_query("SELECT id, title FROM topics WHERE category_id = %s", (category_id,))
     topics = [BaseTopic(id=id, title=title) for id, title in topic_data]
     return CategoryWithTopics(category=category_data, topics=topics)
 
 
-def create(token: str, category: Category):
-    if is_admin(token):
-        generated_id = insert_query("INSERT INTO categories (name) VALUES (?)", (category.name,))
+async def create(token: str, category: Category):
+    if await is_admin(token):
+        generated_id = await insert_query("INSERT INTO categories (name) VALUES (%s)", (category.name,))
         category.id = generated_id
         return category
 
 
-def delete(token: str, category_id: int) -> bool:
-    if is_admin(token):
-        deleted_rows = update_query("DELETE FROM categories WHERE id = ?", (category_id,))
+async def delete(token: str, category_id: int) -> bool:
+    if await is_admin(token):
+        deleted_rows = await update_query("DELETE FROM categories WHERE id = %s", (category_id,))
         return deleted_rows > 0
 
 
-def update(token: str, category_id: int, category: Category) -> UpdateStatus:
-    if is_admin(token):
-        updated_rows = update_query("UPDATE categories SET name = ? WHERE id = ?", (category.name, category_id))
+async def update(token: str, category_id: int, category: Category) -> UpdateStatus:
+    if await is_admin(token):
+        updated_rows = await update_query("UPDATE categories SET name = %s WHERE id = %s", (category.name, category_id))
         return _update_helper(updated_rows, category_id)
 
 
-def lock(token: str, category_id: int) -> UpdateStatus | None:
-    if is_admin(token):
-        updated_rows = update_query("UPDATE categories SET locked = 1 WHERE id = ?", (category_id,))
+async def lock(token: str, category_id: int) -> UpdateStatus | None:
+    if await is_admin(token):
+        updated_rows = await update_query("UPDATE categories SET locked = 1 WHERE id = %s", (category_id,))
         return _update_helper(updated_rows, category_id)
 
 
-def unlock(token: str, category_id: int) -> UpdateStatus:
-    if is_admin(token):
-        updated_rows = update_query("UPDATE categories SET locked = 0 WHERE id = ?", (category_id,))
+async def unlock(token: str, category_id: int) -> UpdateStatus:
+    if await is_admin(token):
+        updated_rows = await update_query("UPDATE categories SET locked = 0 WHERE id = %s", (category_id,))
         return _update_helper(updated_rows, category_id)
 
 
-def make_private(token: str, category_id: int) -> UpdateStatus:
-    if is_admin(token):
-        updated_rows = update_query("UPDATE categories SET is_private = 1 WHERE id = ?", (category_id,))
+async def make_private(token: str, category_id: int) -> UpdateStatus:
+    if await is_admin(token):
+        updated_rows = await update_query("UPDATE categories SET is_private = 1 WHERE id = %s", (category_id,))
         return _update_helper(updated_rows, category_id)
 
 
-def make_non_private(token: str, category_id: int):
-    if is_admin(token):
-        updated_rows = update_query("UPDATE categories SET is_private = 0 WHERE id = ?", (category_id,))
+async def make_non_private(token: str, category_id: int):
+    if await is_admin(token):
+        updated_rows = await update_query("UPDATE categories SET is_private = 0 WHERE id = %s", (category_id,))
         return _update_helper(updated_rows, category_id)
 
 
-def view_privileged_users(token: str, category_id: int):
-    if is_admin(token):
-        privileged_users = read_query(
-            "SELECT user_id, read_access, write_access from categorymembers where category_id =?",
+async def view_privileged_users(token: str, category_id: int):
+    if await is_admin(token):
+        privileged_users = await read_query(
+            "SELECT user_id, read_access, write_access from categorymembers where category_id =%s",
             (category_id,))
         return [PrivilegedCategoryUsers(user_id=user_id, read_access=read_access, write_access=write_access) for
                 user_id, read_access, write_access in privileged_users]
 
 
-def add_user_as_private_member(token: str, category_id: int, user_id: int):
-    if not (category_exists(category_id) and exists_by_id(user_id)):
+async def add_user_as_private_member(token: str, category_id: int, user_id: int):
+    does_category_exist = await category_exists(category_id)
+    does_user_exist = await exists_by_id(user_id)
+    if not (does_category_exist and does_user_exist):
         return UpdateStatus.NOT_FOUND
     if not is_admin(token):
         return UpdateStatus.ADMIN_REQUIRED
 
-    existing_entry = read_query("SELECT * FROM categorymembers WHERE user_id = ? AND category_id = ?",
-                                (user_id, category_id))
+    existing_entry = await read_query("SELECT * FROM categorymembers WHERE user_id = %s AND category_id = %s",
+                                      (user_id, category_id))
     if existing_entry:
         return UpdateStatus.DUPLICATE_ENTRY
 
-    insert_query("INSERT INTO categorymembers (user_id, category_id, read_access) VALUES (?,?,?)",
-                 (user_id, category_id, 1))
+    await insert_query("INSERT INTO categorymembers (user_id, category_id, read_access) VALUES (%s,%s,%s)",
+                       (user_id, category_id, 1))
 
     return UpdateStatus.SUCCESS
 
 
-def remove_user_as_private_member(token: str, category_id: int, user_id: int):
-    if not (category_exists(category_id) and exists_by_id(user_id)):
+async def remove_user_as_private_member(token: str, category_id: int, user_id: int):
+    if not (await category_exists(category_id) and await exists_by_id(user_id)):
         return UpdateStatus.NOT_FOUND
     if not is_admin(token):
         return UpdateStatus.ADMIN_REQUIRED
-    affected_rows = update_query("DELETE FROM categorymembers WHERE user_id = ? and category_id = ?",
-                                 (user_id, category_id))
+    affected_rows = await update_query("DELETE FROM categorymembers WHERE user_id = %s and category_id = %s",
+                                       (user_id, category_id))
     if affected_rows == 0:
         return UpdateStatus.NOT_FOUND
     return UpdateStatus.SUCCESS
@@ -112,7 +116,7 @@ def remove_user_as_private_member(token: str, category_id: int, user_id: int):
 
 def _update_helper(updated_rows: int, category_id: int) -> UpdateStatus:
     if updated_rows == 0:
-        category = category_exists(category_id)
+        category = await category_exists(category_id)
         if category is None:
             return UpdateStatus.NOT_FOUND
         else:
@@ -120,32 +124,24 @@ def _update_helper(updated_rows: int, category_id: int) -> UpdateStatus:
     return UpdateStatus.SUCCESS
 
 
-def category_exists(category_id: int) -> Category | None:
-    data = read_query("SELECT * FROM categories WHERE id = ?", (category_id,))
+async def category_exists(category_id: int) -> Category | None:
+    data = await read_query("SELECT * FROM categories WHERE id = %s", (category_id,))
     if not data:
         return None
     return CategoryOut(id=data[0][0], name=data[0][1], private=data[0][2], locked=data[0][3])
 
 
-def _check_duplicate_name(category_id: int, new_name: str) -> bool:
-    # todo: double check if needed or if the database will have unique constraint for duplicates
-    data = read_query("SELECT name FROM categories WHERE id = ?", (category_id,))
-    if not data:
-        return False
-    return data[0][0].lower() == new_name.lower()
+async def is_category_private(category_id: int):
+    return await read_query("SELECT is_private FROM categories WHERE id = %s", (category_id,))
 
 
-def is_category_private(category_id: int):
-    return read_query("SELECT is_private FROM categories WHERE id = ?", (category_id,))
+async def category_read_restriction_applies(user_id: int):
+    return await read_query("SELECT read_access FROM categorymembers WHERE user_id = %s", (user_id,))
 
 
-def category_read_restriction_applies(user_id: int):
-    return read_query("SELECT read_access FROM categorymembers WHERE user_id = ?", (user_id,))
+async def category_write_restriction_applies(user_id: int):
+    return await read_query("SELECT write_access FROM categorymembers WHERE user_id = %s", (user_id,))
 
 
-def category_write_restriction_applies(user_id: int):
-    return read_query("SELECT write_access FROM categorymembers WHERE user_id = ?", (user_id,))
-
-
-def is_category_locked(category_id: int):
-    return read_query("SELECT locked FROM categories WHERE id = ?", (category_id,))
+async def is_category_locked(category_id: int):
+    return await read_query("SELECT locked FROM categories WHERE id = %s", (category_id,))
