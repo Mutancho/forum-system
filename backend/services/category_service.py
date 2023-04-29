30c29d4b66667dd77403051898740352f38e4ef3
DimitarP <dpolihronov1@gmail.com>
2023-04-29 19:25:17 +0300

    fix remove_user_as_private_member and add_user_as_private_member by adding await to is_admin

--- backend/services/category_service.py
+++ backend/services/category_service.py
@@ -88,7 +88,8 @@ async def add_user_as_private_member(token: str, category_id: int, user_id: int)
     does_user_exist = await exists_by_id(user_id)
     if not (does_category_exist and does_user_exist):
         return UpdateStatus.NOT_FOUND
-    if not is_admin(token):
+
+    if not await is_admin(token):
         return UpdateStatus.ADMIN_REQUIRED
 
     existing_entry = await read_query("SELECT * FROM categorymembers WHERE user_id = %s AND category_id = %s",
@@ -105,7 +106,7 @@ async def add_user_as_private_member(token: str, category_id: int, user_id: int)
 async def remove_user_as_private_member(token: str, category_id: int, user_id: int):
     if not (await category_exists(category_id) and await exists_by_id(user_id)):
         return UpdateStatus.NOT_FOUND
-    if not is_admin(token):
+    if not await is_admin(token):
         return UpdateStatus.ADMIN_REQUIRED
     affected_rows = await update_query("DELETE FROM categorymembers WHERE user_id = %s and category_id = %s",
                                        (user_id, category_id))
