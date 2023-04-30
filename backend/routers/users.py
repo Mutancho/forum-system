c8280b835f6747784b2bd4da3be3209312a1ffb0
DimitarP <dpolihronov1@gmail.com>
2023-04-30 20:48:37 +0300

    add asynchronous functionality

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -2,60 +2,61 @@ from fastapi import APIRouter, Response, Header
 from fastapi.security import OAuth2PasswordBearer
 from schemas.user import User, EmailLogin, UsernameLogin, Member, RevokeMemberAccess, UpdateUser
 from services import user_service, category_service
+from services.user_service import is_admin
 from utils import oauth2
 
 router_user = APIRouter(prefix='/users', tags=['Users'])
 
 
 @router_user.get('/')
-def get_all_users(Authorization: str = Header()):
+async def get_all_users(Authorization: str = Header()):
     token = Authorization[8:-1]
-    if not user_service.is_admin(token):
+    if not await user_service.is_admin(token):
         return Response(status_code=403)
-    return user_service.all()
+    return await user_service.all()
 
 
 @router_user.get('/{id}')
-def get_user_bt_id(id: int, Authorization: str = Header()):
+async def get_user_bt_id(id: int, Authorization: str = Header()):
     token = Authorization[8:-1]
-    if not user_service.exists_by_id(id):
+    if not await user_service.exists_by_id(id):
         return Response(status_code=404)
-    if not user_service.is_user_authorized_to_get_delete(token, id):
+    if not await user_service.is_user_authorized_to_get_delete(token, id):
         return Response(status_code=403)
-    return user_service.get_by_id(id)
+    return await user_service.get_by_id(id)
 
 
 @router_user.post('/', status_code=201)
-def create_user(user: User):
-    if user_service.exists_by_username_email(user):
+async def create_user(user: User):
+    if await user_service.exists_by_username_email(user):
         return Response(status_code=400,
                         content=f'A User with this username: {user.username} and email: {user.email} already exists!')
 
-    return user_service.create(user)
+    return await user_service.create(user)
 
 
 @router_user.put('/{id}')
-def update_user(id: int, user: UpdateUser, Authorization: str = Header()):
+async def update_user(id: int, user: UpdateUser, Authorization: str = Header()):
     token = Authorization[8:-1]
-    auth_user_id = oauth2.get_current_user(token)
-    if not user_service.exists_by_id(id):
+    user_id = oauth2.get_current_user(token)
+    if not await user_service.exists_by_id(id):
         return Response(status_code=404)
-    if id != auth_user_id:
+    if id != user_id or not is_admin(user_id):
         return Response(status_code=403)
-    if user_service.check_unique_update_email_password(id, user):
+    if await user_service.check_unique_update_email_password(id, user):
         return Response(status_code=400, content=f'A User with this username or email already exists!')
 
-    return user_service.update(id, user)
+    return await user_service.update(id, user)
 
 
 @router_user.delete('/{id}')
-def delete_user(id: int, Authorization: str = Header()):
+async def delete_user(id: int, Authorization: str = Header()):
     token = Authorization[8:-1]
-    if not user_service.exists_by_id(id):
+    if not await user_service.exists_by_id(id):
         return Response(status_code=404)
-    if not user_service.is_user_authorized_to_get_delete(token, id):
+    if not await user_service.is_user_authorized_to_get_delete(token, id):
         return Response(status_code=403)
-    user_service.delete(id)
+    await user_service.delete(id)
     return Response(status_code=204)
 
 
@@ -73,28 +74,31 @@ async def login(credentials: EmailLogin | UsernameLogin):
 
 
 @router_user.post('/give_access')
-def give_access(member_access: Member, Authorization: str = Header()):
+async def give_access(member_access: Member, Authorization: str = Header()):
+    # todo test to ensure it works
     token = Authorization[8:-1]
     if not user_service.is_admin(token):
         return Response(status_code=403)
-    if not user_service.exists_by_id(member_access.user_id):
+    if not await user_service.exists_by_id(member_access.user_id):
         return Response(status_code=400, content=f'User with id: {member_access.user_id} does not exist!')
-    if category_service.category_exists(member_access.category_id) == None:
+    if await category_service.category_exists(member_access.category_id) is None:
         return Response(status_code=400, content=f'Category with id: {member_access.category_id} does not exist!')
 
-    user_service.give_access(member_access)
+    await user_service.give_access(member_access)
     return Response(status_code=200)
 
 
-@router_user.post('/revoke_access')
-def revoke_access(member_access: RevokeMemberAccess, Authorization: str = Header()):
+@router_user.put('/revoke_access')
+async def revoke_access(member_access: RevokeMemberAccess, Authorization: str = Header()):
+    print(32)
+    # todo test to ensure it works
     token = Authorization[8:-1]
     if not user_service.is_admin(token):
         return Response(status_code=403)
-    if not user_service.exists_by_id(member_access.user_id):
+    if not await user_service.exists_by_id(member_access.user_id):
         return Response(status_code=400, content=f'User with id: {member_access.user_id} does not exist!')
-    if not user_service.user_has_permissions_for_category(member_access.user_id, member_access.category_id):
+    if not await user_service.user_has_permissions_for_category(member_access.user_id, member_access.category_id):
         return Response(status_code=404)
 
-    user_service.revoke_access(member_access)
+    await user_service.revoke_access(member_access)
     return Response(status_code=200)
--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -1,61 +1,61 @@
-import datetime
 from utils import oauth2
 from schemas.user import User, EmailLogin, UsernameLogin, Member, RevokeMemberAccess, UpdateUser
 from database.database_queries import insert_query, read_query, update_query
-import bcrypt
+from utils.passwords import hash_password, verify_password
 
 
-def all():
-    data = read_query('''SELECT id, username, email, role, created_at FROM users''')
+async def all():
+    data = await read_query('''SELECT id, username, email, role, created_at FROM users''')
 
     return (User.from_query_result(*u) for u in data)
 
 
-def create(user: User):
-    hashed = _hash_password(user.password)
+async def create(user: User):
+    hashed = await hash_password(user.password)
 
-    generate_id = insert_query('''INSERT INTO users(username,email,password,role) VALUES (?,?,?,?)''',
-                               (user.username, user.email, hashed, user.role))
+    generate_id = await insert_query('''INSERT INTO users(username,email,password,role) VALUES (%s,%s,%s,%s)''',
+                                     (user.username, user.email, hashed, user.role))
     user.id = generate_id
-    user.created_at = read_query('''SELECT created_at FROM users WHERE id = ?''', (generate_id,))[0][0]
+    created_at = await read_query('''SELECT created_at FROM users WHERE id = %s''', (generate_id,))
+    user.created_at = created_at[0][0]
     return user
 
 
-def update(id: int, user: UpdateUser):
-    password = _hash_password(user.password)
-    update_query('''UPDATE users SET username = ?, email = ? , password = ? WHERE id = ?  ''',
-                 (user.username, user.email, password, id))
+async def update(id: int, user: UpdateUser):
+    password = await hash_password(user.password)
+    await update_query('''UPDATE users SET username = %s, email = %s , password = %s WHERE id = %s  ''',
+                       (user.username, user.email, password, id))
 
     return user
 
 
-def check_unique_update_email_password(id: int, user: UpdateUser):
-    data = read_query('''SELECT username,email FROM users WHERE (username = ? OR email = ?) AND id <> ?''',
-                      (user.username, user.email, id))
+async def check_unique_update_email_password(id: int, user: UpdateUser):
+    data = await read_query('''SELECT username,email FROM users WHERE (username = %s OR email = %s) AND id <> %s''',
+                            (user.username, user.email, id))
 
     return len(data) > 0
 
 
-def delete(id: int):
-    data = update_query('''DELETE FROM users WHERE id = ?''', (id,))
+async def delete(id: int):
+    await update_query('''DELETE FROM users WHERE id = %s''', (id,))
 
 
-def get_by_id(id: int):
-    data = read_query('''SELECT id, username, email, role, created_at FROM users WHERE id = ?''',
-                      (id,))
+async def get_by_id(id: int):
+    data = await read_query('''SELECT id, username, email, role, created_at FROM users WHERE id = %s''',
+                            (id,))
     return User.from_query_result(*data[0])
 
 
-def exists_by_username_email(user: User):
-    data = read_query('''SELECT username,email FROM users WHERE username =? or email = ?''',
-                      (user.username, user.email))
+async def exists_by_username_email(user: User):
+    data = await read_query('''SELECT username,email FROM users WHERE username =%s or email = %s''',
+                            (user.username, user.email))
 
     return len(data) > 0
 
 
 async def exists_by_id(id: int):
     data = await read_query('''SELECT id FROM users WHERE id = %s''',
-                      (id,))
+                            (id,))
 
     return len(data) > 0
 
@@ -75,35 +75,38 @@ async def login(credentials: EmailLogin | UsernameLogin):
     return oauth2.create_access_token(id)
 
 
-def give_access(member_access: Member):
+async def give_access(member_access: Member):
     read_access = int(member_access.read_access)
     write_access = int(member_access.write_access)
     if user_has_permissions_for_category(member_access.user_id, member_access.category_id):
-        update_query(
-            '''UPDATE categorymembers SET read_access = ?,write_access = ?  Where user_id = ? and category_id = ?''',
+        await update_query(
+            '''UPDATE categorymembers SET read_access = %s,write_access = %s  Where user_id = %s and category_id = %s''',
             (read_access, write_access, member_access.user_id, member_access.category_id))
     else:
-        insert_query('''INSERT INTO categorymembers(user_id,category_id,read_access,write_access) VALUES (?,?,?,?)''',
-                     (member_access.user_id, member_access.category_id, read_access, write_access))
+        await insert_query(
+            '''INSERT INTO categorymembers(user_id,category_id,read_access,write_access) VALUES (%s,%s,%s,%s)''',
+            (member_access.user_id, member_access.category_id, read_access, write_access))
 
 
-def revoke_access(member_access: RevokeMemberAccess):
+async def revoke_access(member_access: RevokeMemberAccess):
     read_access = 0
     write_access = 0
-    update_query(
-        '''UPDATE categorymembers SET read_access = ?,write_access = ?  Where user_id = ? and category_id = ?''',
+    await update_query(
+        '''UPDATE categorymembers SET read_access = %s,write_access = %s  Where user_id = %s and category_id = %s''',
         (read_access, write_access, member_access.user_id, member_access.category_id))
 
 
-def user_has_permissions_for_category(user_id: int, category_id: int):
-    data = read_query('''SELECT * FROM categorymembers WHERE user_id = ? and category_id = ?''', (user_id, category_id))
+async def user_has_permissions_for_category(user_id: int, category_id: int):
+    data = await read_query('''SELECT * FROM categorymembers WHERE user_id = %s and category_id = %s''',
+                            (user_id, category_id))
 
     return len(data) > 0
 
 
 async def verify_credentials(credentials: EmailLogin | UsernameLogin):
+    data = None
     if isinstance(credentials, EmailLogin):
-        data = await read_query('''SELECT username,email,password FROM users WHERE email = ?''',
+        data = await read_query('''SELECT username,email,password FROM users WHERE email = %s''',
                                 (credentials.email,))
     if isinstance(credentials, UsernameLogin):
         data = await read_query('''SELECT username,email,password FROM users WHERE username = %s''',
@@ -111,10 +114,10 @@ async def verify_credentials(credentials: EmailLogin | UsernameLogin):
     return len(data) > 0
 
 
-def is_user_authorized_to_get_delete(token: str, id: int):
+async def is_user_authorized_to_get_delete(token: str, id: int):
     user_id = oauth2.get_current_user(token)
-    data = read_query('''SELECT role FROM users WHERE id = ?''',
-                      (user_id,))
+    data = await read_query('''SELECT role FROM users WHERE id = %s''',
+                            (user_id,))
     role = data[0][0]
     return user_id == id or role.lower() == 'admin'
 
@@ -122,23 +125,18 @@ def is_user_authorized_to_get_delete(token: str, id: int):
 async def is_admin(token: str):
     user_id = oauth2.get_current_user(token)
     data = await read_query('''SELECT role FROM users WHERE id = %s''',
-                      (user_id,))
+                            (user_id,))
     role = data[0][0]
     return role.lower() == 'admin'
 
 
-def _hash_password(password: str):
-    salt = b'$2b$12$V0NmXBYEU2o0x3nbxOPouu'
-    return bcrypt.hashpw(password.encode('utf-8'), salt)
-
-
 async def valid_password(credentials: EmailLogin | UsernameLogin):
-    hashed = _hash_password(credentials.password)
+    actual_password = None
     if isinstance(credentials, EmailLogin):
-        result = await read_query('''SELECT password FROM users WHERE  email = %s''', (credentials.email,))[0][0]
+        result = await read_query('''SELECT password FROM users WHERE  email = %s''', (credentials.email,))
         actual_password = result[0][0]
-    if isinstance(credentials, UsernameLogin):
+    elif isinstance(credentials, UsernameLogin):
         result = await read_query('''SELECT password FROM users WHERE username = %s ''', (credentials.username,))
         actual_password = result[0][0]
 
-    return hashed.decode() == actual_password
+    return await verify_password(credentials.password, actual_password)
