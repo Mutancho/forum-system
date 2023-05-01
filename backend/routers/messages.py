e56e08f68b366fe9e3847d98e731a3ea125fbc88
DimitarP <dpolihronov1@gmail.com>
2023-05-01 14:20:52 +0300

    add asynchronous functionality

--- backend/routers/messages.py
+++ backend/routers/messages.py
@@ -1,33 +1,32 @@
-from fastapi import APIRouter, Response,Header
+from fastapi import APIRouter, Response, Header
 from fastapi.security import OAuth2PasswordBearer
 from schemas.message import Message
-from services import message_service,user_service
+from services import message_service, user_service
 from utils import oauth2
 
-router_message = APIRouter(prefix='/messages',tags=['Messages'])
+router_message = APIRouter(prefix='/messages', tags=['Messages'])
+
 
 @router_message.post('/')
-def create_message(message: Message,Authorization:str = Header()):
+async def create_message(message: Message, Authorization: str = Header()):
     token = Authorization[8:-1]
-    message.sender_id=oauth2.get_current_user(token)
-    if user_service.exists_by_id(message.sender_id) and user_service.exists_by_id(message.reciever_id):
-        return message_service.create(message)
+    message.sender_id = oauth2.get_current_user(token)
+    if await user_service.exists_by_id(message.sender_id) and await user_service.exists_by_id(message.receiver_id):
+        return await message_service.create(message)
 
-    return Response(status_code=400,content="Sender or Receiver id doesn't match an existing user")
-
+    return Response(status_code=400, content="Sender or Receiver id doesn't match an existing user")
 
 
 @router_message.get('/conversations')
-def get_conversations(Authorization:str = Header()):
+async def get_conversations(Authorization: str = Header()):
     token = Authorization[8:-1]
-    print(user_service.is_admin(token))
-    return message_service.get_all_my_conversations(token)
+    return await message_service.get_all_my_conversations(token)
+
 
 @router_message.get('/conversation/{id}')
-def get_conversations_with(id: int,Authorization:str = Header()):
+async def get_conversations_with(id: int, Authorization: str = Header()):
     token = Authorization[8:-1]
-    if not user_service.exists_by_id(id):
+    if not await user_service.exists_by_id(id):
         return Response(status_code=404)
 
-    return message_service.get_all_my_conversations_with(token,id)
-
+    return await message_service.get_all_my_conversations_with(token, id)
--- backend/services/message_service.py
+++ backend/services/message_service.py
@@ -1,37 +1,40 @@
-import datetime
 from schemas.message import Message
-from database.database_queries import insert_query, read_query, update_query
-from schemas.user import User,DisplayUser
+from database.database_queries import insert_query, read_query
+from schemas.user import DisplayUser
 from utils import oauth2
 
 
-def create(message: Message):
-    generate_id = insert_query('''INSERT INTO messages(content,sender_id,receiver_id) VALUES (?,?,?)''',(message.content,message.sender_id,message.reciever_id))
+async def create(message: Message):
+    generate_id = await insert_query('''INSERT INTO messages(content,sender_id,receiver_id) VALUES (%s,%s,%s)''',
+                                     (message.content, message.sender_id, message.receiver_id))
 
     message.id = generate_id
-    message.created_at = read_query('''SELECT created_at FROM message WHERE id = ?''',(generate_id,))[0]
-
+    created_at = await read_query('''SELECT created_at FROM messages WHERE id = %s''', (generate_id,))
+    message.created_at = created_at[0]
     return message
 
-def get_all_my_conversations(token:str)->list[DisplayUser]:
+
+async def get_all_my_conversations(token: str) -> list[DisplayUser]:
     id = oauth2.get_current_user(token)
-    data = read_query('''SELECT receiver_id FROM messages WHERE sender_id = ? ''',(id,))
-    data += read_query('''SELECT sender_id FROM messages WHERE receiver_id  = ? ''', (id,))
+    data = await read_query('''SELECT receiver_id FROM messages WHERE sender_id = %s ''', (id,))
+    data += await read_query('''SELECT sender_id FROM messages WHERE receiver_id  = %s ''', (id,))
     ids = ','.join(str(id[0]) for id in set(data))
+    users = await read_query(
+        f'''SELECT id, username, email, password, role, created_at FROM users WHERE id in ({ids})''')
+    return [DisplayUser.from_query_result(*u) for u in users]
 
-    users = read_query(f'''SELECT * FROM users WHERE id in ({ids})''')
 
-    return (DisplayUser.from_query_result(*u) for u in users)
-
-def get_all_my_conversations_with(token:str,receiver_id:int):#The formating needs to be fixed so JSON displayes it as intended
+async def get_all_my_conversations_with(token: str,
+                                        receiver_id: int):  # The formating needs to be fixed so JSON displayes it as intended
     auth_user_id = oauth2.get_current_user(token)
-    data = read_query('''SELECT sender_id,content,created_at,receiver_id FROM messages WHERE (sender_id=? and receiver_id = ?) or 
-    (receiver_id = ? and sender_id = ?) ORDER BY created_at''',(auth_user_id,receiver_id,auth_user_id,receiver_id))
-    converstion = {}
+    data = await read_query('''SELECT sender_id,content,created_at,receiver_id FROM messages WHERE (sender_id=%s and receiver_id = %s) or 
+    (receiver_id = %s and sender_id = %s) ORDER BY created_at''',
+                            (auth_user_id, receiver_id, auth_user_id, receiver_id))
+    conversation = {}
     for msg in data:
-        if msg[0]== auth_user_id:
-            converstion[msg[2].strftime("%m/%d/%Y, %H:%M:%S")]= msg[1]
+        if msg[0] == auth_user_id:
+            conversation[msg[2].strftime("%m/%d/%Y, %H:%M:%S")] = msg[1]
         else:
-            converstion[msg[2].strftime("%m/%d/%Y, %H:%M:%S")] = '         '+msg[1]
+            conversation[msg[2].strftime("%m/%d/%Y, %H:%M:%S")] = '         ' + msg[1]
 
-    return converstion
+    return conversation
