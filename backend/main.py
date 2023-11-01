cc9a452ba0f0f6e2331a42b1ad276c06f126c782
DimitarP <dpolihronov1@gmail.com>
2023-11-01 08:03:54 +0200

    add middleware

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from routers.messages import router_message
 from routers.topics import router_topic, router_all_topics
 from routers.replies import router_reply
 from database.connection import init_db, get_connection
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -19,6 +20,18 @@ async def shutdown_event():
     pool = await get_connection()
     await pool.close()
 
+
+origins = [
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_user)
 app.include_router(router_category)
